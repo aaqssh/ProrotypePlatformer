4d1f62dcc4c9cc05bd47a808bb329cfeb848aa2c
Akash Rauniyar <146624602+aaqssh@users.noreply.github.com>
2026-04-08 10:16:02 +0300

    deletedAddedCodePlayer MOvement

--- Source/ProrotypePlatformer/Private/BombJackHero.cpp
+++ Source/ProrotypePlatformer/Private/BombJackHero.cpp
@@ -1,34 +1,82 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "BombJackHero.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
-// Sets default values
+// these were values that i used in blueprint first and putting this in code now
 ABombJackHero::ABombJackHero()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//Just start things
 	PrimaryActorTick.bCanEverTick = true;
-
+	
+	//For Spring Arm
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("MySpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = true;
+	SpringArm-> TargetArmLength = 2000.0f;
+	
+	//For camera 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("MyCamera"));
+	Camera->SetupAttachment(SpringArm);
+	
+	Camera->SetRelativeLocation(FVector(0.0f, 0.0f, 8.492264f));
+	Camera->SetRelativeRotation(FRotator(0.0f, 0.0f, -90.0f));
+	
+	//Just fixing max speed to 500 and additional default setting change to true
+	GetCharacterMovement()->MaxWalkSpeed=500.0f;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
-// Called when the game starts or when spawned
+// After setting Enhanced Input then this 
 void ABombJackHero::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	APlayerController* BombJackController = Cast<APlayerController>(GetController());
+	
+	if (BombJackController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(BombJackController->GetLocalPlayer());
+		
+		if (Subsystem)
+		{
+			Subsystem->ClearAllMappings();
+			Subsystem->AddMappingContext(IMCBombJack,0);
+		}
+	}
 }
 
-// Called every frame
+
+
+// Runs every frame for gameplay
 void ABombJackHero::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
+// bind functionality to input
 void ABombJackHero::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered, this, &ABombJackHero::Move);
+	}
 
 }
 
+void ABombJackHero::Move(const FInputActionValue& Value)
+{
+	FVector2D RCVValue = Value.Get<FVector2D>();
+	
+	FRotator ControlRotation = GetControlRotation();
+	FVector FrontVector = FRotationMatrix(FRotator(0,ControlRotation.Yaw,0)).GetUnitAxis(EAxis::X);
+	AddMovementInput(FrontVector,RCVValue.Y);
+}
--- Source/ProrotypePlatformer/Public/BombJackHero.h
+++ Source/ProrotypePlatformer/Public/BombJackHero.h
@@ -1,9 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Let us start with spring component, camera to Character and go on later to add more
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
 #include "BombJackHero.generated.h"
 
 UCLASS()
@@ -14,16 +16,27 @@ class PROROTYPEPLATFORMER_API ABombJackHero : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ABombJackHero();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, Category = "mInput")
+	UInputMappingContext* IMCBombJack;
+	
+	UPROPERTY(EditAnywhere, Category = "mInput")
+	UInputAction* MoveAction;
+	
+	void Move(const FInputActionValue& Value );
+private:
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+	
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
 };
